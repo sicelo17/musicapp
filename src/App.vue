bc4420febda96343e53eed4b1f2e4ac235a36b7c
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-01-25 01:24:56 +0200

    functionality

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,17 @@
       <section class="player">
         <h2 class="song-title">{{current.title}} - <span>{{ current.artist}}</span></h2>
         <div class="controls">
-          <button class="prev">Prev</button>
-          <button class="play" v-if="!isPlaying">Play</button>
-          <button class="pause" v-else>Pause</button>
-          <button class="next">Next</button>
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
         </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{song.title}} - {{song.artist}}
+        </button>
       </section>
     </main>
   </div>
@@ -51,6 +57,44 @@ export default {
         }
       ],
       player: new Audio()
+    }
+  },
+  methods: {
+    play(song) {
+      if(typeof song.src != "undefined"){
+        this.current = song
+        this.player.src = this.current.src
+      }
+      this.player.play()
+      this.player.addEventListener('ended', function () {
+        this.index++
+      if(this.index > this.songs.length - 1){
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+      }.bind(this));
+      this.isPlaying = true
+    },
+    pause() {
+      this.player.pause()
+      this.isPlaying = false
+    },
+    next(){
+      this.index++
+      if(this.index > this.songs.length - 1){
+        this.index = 0
+      }
+      this.current = this.songs[this.index]
+      this.play(this.current)
+    },
+    prev() {
+      this.index--
+      if(this.index < 0){
+        this.index = this.songs.length - 1
+      }
+      this.current = this.songs[this.index]
+      this.play(this.current)
     }
   },
   created() {
